ce6b3d155367db28a80ca9a183cb0607ea6da7a0
rewrecko <ptonigame@gmail.com>
2023-07-24 13:22:47 +0200

    php captcha review

--- captcha/captcha_generator.php
+++ captcha/captcha_generator.php
@@ -8,15 +8,12 @@ $randomString = substr(md5(rand()), 0, 5);
 $_SESSION['captcha'] = $randomString;
 
 // Crea un'immagine vuota per il CAPTCHA
-$captchaImage = imagecreatefrompng('captcha_background.png'); // Assicurati di avere un'immagine di sfondo valida
+$captchaImage = imagecreate(100, 40); // Imposta le dimensioni dell'immagine
+$backgroundColor = imagecolorallocate($captchaImage, 255, 255, 255); // Sfondo bianco
+$textColor = imagecolorallocate($captchaImage, 0, 0, 0); // Testo nero
 
-// Imposta il colore del testo (nero)
-$textColor = imagecolorallocate($captchaImage, 0, 0, 0);
-
-// Imposta il testo sulle coordinate dell'immagine (posizione casuale per aumentare la sicurezza)
-$textX = rand(10, 30);
-$textY = rand(20, 40);
-imagestring($captchaImage, 5, $textX, $textY, $randomString, $textColor);
+// Disegna il testo del CAPTCHA sull'immagine
+imagestring($captchaImage, 5, 25, 12, $randomString, $textColor);
 
 // Imposta l'intestazione dell'immagine
 header('Content-type: image/png');
